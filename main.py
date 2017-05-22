3d2dfdda60c3bfb1b15f658b71c457cd7ccdda5c
Dmitry Andreev <d.andreev@skoltech.ru>
2017-05-22 15:28:24 +0300

    refactoring

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import factory
 from formula import Formula
 
 if __name__ == "__main__":
-    text_formula = input('Input formula to eval. \nImplemented operators: {}\n'.format(factory.operators_seq))
+    text_formula = input('Input formula to eval. \nImplemented operators: {}\n'.format('|'.join([op for op in factory.operators])))
     print(Formula(text_formula).eval())
